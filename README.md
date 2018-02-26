92a4b26bfaabcf8c450dd449267a55c8344f83dd
Brennan Ackerman <b.ackerman@protonmail.ch>
2018-02-25 21:35:28 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Eye Direction Detection
+## Python and OpenCV Haarcascades
+
+### Current Issues
+1. Current environment in VM on Hypervisor and cannot use USB camera
+2. Camera connected via URL (RTMP/RTSP streams) very slow
---
+++ eyedetection.py
@@ -0,0 +1,28 @@
+import cv2
+import numpy as np
+
+import config
+
+# Define haar cascades
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+# Load webcam feed
+cap = cv2.VideoCapture(config.CAMERA_URL)
+
+ret, img = cap.read()
+
+while True:
+    ret, img = cap.read()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img, (x,y), (x+w, y+h), (255, 0, 0), 2)
+    
+    cv2.imshow('img', img)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
